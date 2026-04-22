30447a90a2c8de56dc90eb470319ad2461822da1
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2026-04-22 22:46:45 +0200

    Create a modal component

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,30 @@
+import { CheckCircle, AlertOctagon } from "lucide-react";
+
+export default function Modal(
+  isOpen,
+  type,
+  title,
+  message,
+  titleButton,
+  action,
+) {
+  if (!isOpen) return null;
+
+  const iconMap = {
+    success: <CheckCircle />,
+    error: <AlertOctagon />,
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-icon">{iconMap[type]}</div>
+        <h2 className="modal-title">{title}</h2>
+        <p className="modal-message">{message}</p>
+        <button className="modal-button" onClick={action}>
+          {titleButton}
+        </button>
+      </div>
+    </div>
+  );
+}
