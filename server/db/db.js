d89c97b998eecb00cd5e0bd2d5f1c69a8304ee4f
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2026-04-23 19:31:32 +0200

    Update connecting to the database

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,23 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-module.exports = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let pool;
+
+if (process.env.NODE_ENV === "production") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
+
+module.exports = pool;
