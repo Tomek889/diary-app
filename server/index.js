875942e8e2e814a174813f3400996b85dbc276cb
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2026-04-23 20:26:43 +0200

    Update CORS policies

--- server/index.js
+++ server/index.js
@@ -17,8 +17,22 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://diary-app-jet.vercel.app",
+];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }),
+);
 app.use(express.json());
 
 app.get("/api/dates", async (req, res) => {
